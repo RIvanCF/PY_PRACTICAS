d2efa85d6f3ca3f30bd9ae4121ea0708383c60ca
RIvanCF <flores.ivan940214@gmail.com>
2024-06-02 21:05:25 -0600

    Nueva Practica: input_dato

---
+++ input_dato.py
@@ -0,0 +1,6 @@
+'''El ingreso de valores de cualquier tipo es esencial en la iplementacion de programas cuyos valores sean 
+variables y de libre definición, basicamente es ingresar un dato desde el teclado 
+'''
+dato_in = input("Ingresa un dato: ") #Ingreso de datos desde el teclado, EL VALOR INGRESADO RETORNARA CON UN VALOR STR
+print("El valor que ingresaste es: ", dato_in)#Impresión del dato
+

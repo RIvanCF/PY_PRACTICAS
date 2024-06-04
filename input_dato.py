14d504db5d82f335ac65a09aa9deca70756ddd38
RIvanCF <flores.ivan940214@gmail.com>
2024-06-03 22:11:25 -0600

    Nuevas Lineas de codigo en input_dato

--- input_dato.py
+++ input_dato.py
@@ -4,3 +4,8 @@ variables y de libre definición, basicamente es ingresar un dato desde el tecla
 dato_in = input("Ingresa un dato: ") #Ingreso de datos desde el teclado, EL VALOR INGRESADO RETORNARA CON UN VALOR STR
 print("El valor que ingresaste es: ", dato_in)#Impresión del dato
 
+'''Para que el dato ingresado tenga las propiedades deseadas, se debe definir el tipo de dato correspondiente'''
+dato_input = input("Ingresa un dato del tipo entero: ") #Aqui el dato a ingresar sera del tipo string (str)
+dato_input = int(dato_input)#El dato pasa a convertise en un valor del tipo entero (int)
+print("Dato ingresado: ", dato_input)
+print("Es un valor del tipo: ",type(dato_input))
